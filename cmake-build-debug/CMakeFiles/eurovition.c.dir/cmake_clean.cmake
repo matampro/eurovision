352505354cd1f74bd1842c7e91aaf5ecfc83267a
ayalaavital <49597407+ayalaavital@users.noreply.github.com>
2019-04-30 17:26:50 +0300

    .

---
+++ cmake-build-debug/CMakeFiles/eurovition.c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eurovition.c.dir/map.c.obj"
+  "eurovition.c.pdb"
+  "eurovition.c.exe"
+  "eurovition.c.exe.manifest"
+  "libeurovition.c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/eurovition.c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
